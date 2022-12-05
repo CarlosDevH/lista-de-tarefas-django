fabf4fb0a123321052bdd1d9ec8972366f35670f
Carlos Henrique Magalhães <carlosdeveloper669@gmail.com>
2022-12-05 02:57:36 -0300

    Second commit of the django-list

--- tasks/views.py
+++ tasks/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def helloWorld(request):
     return HttpResponse('Hello world!')
+
+def taskList(request):
+    return render(request, 'tasks/list.html')
+
+def yourName(resquest, name):
+    return render(resquest, 'tasks/yourname.html', {'name': name})
